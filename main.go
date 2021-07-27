d01602290ac47df51a521541008d4aad6d83186f
sumerpunjabi <64615400+sumerpunjabi@users.noreply.github.com>
2021-07-27 14:03:57 +0530

    Added better commenting

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 // getRepos returns an array of repository names on codacy
 func getRepos() []string{
 
+	// struct to receive and breakdown repository list json data
 	type repoResults struct {
 		Data []struct {
 			Repository struct {
@@ -31,9 +32,9 @@ func getRepos() []string{
 	req.Header = map[string][]string{
 		"Accept": {"application/json"},
 		"api-token": {"9BdbyQFW4T4MCftcWvWw"},
-	}
+	} // provides auth to http request
 
-	client := &http.Client{Timeout: 10 * time.Second}
+	client := &http.Client{Timeout: 10 * time.Second} // http client times out to prevent getting stuck while making request
 	resp, err := client.Do(req)
 	if err != nil{
 		log.Fatalln(err)
@@ -46,20 +47,25 @@ func getRepos() []string{
 	}
 
 	res := &repoResults{}
-	err = json.Unmarshal(body, res)
-
-	var temp []string
-
-	for _ , v := range res.Data {
-		temp = append(temp, v.Repository.Name)
+	err = json.Unmarshal(body, res)	// translates json response body into struct
+	if err != nil{
+		log.Fatalln(err)
 	}
 
-	return temp
+	var repoList []string
+
+	// makes slice of repository names to return
+	for _ , v := range res.Data {
+		repoList = append(repoList, v.Repository.Name)
+	}
+
+	return repoList
 }
 
 // issues returns a buffered integer channel with issues of the respective repo
 func issues(name string, chan1 chan int) {
 
+	// struct to receive and breakdown repository issue json data
 	type Results struct {
 		Data []struct {
 			Category struct {
@@ -76,9 +82,9 @@ func issues(name string, chan1 chan int) {
 	req.Header = map[string][]string{
 		"Accept": {"application/json"},
 		"api-token": {"9BdbyQFW4T4MCftcWvWw"},
-	}
+	}	// provides auth to http request
 
-	client := &http.Client{Timeout: 10 * time.Second}
+	client := &http.Client{Timeout: 10 * time.Second}	// http client times out to prevent getting stuck while making request
 	resp, err := client.Do(req)
 	if err != nil{
 		log.Fatalln(err)
@@ -91,8 +97,9 @@ func issues(name string, chan1 chan int) {
 	}
 
 	res := &Results{}
-	err = json.Unmarshal(body, res)
+	err = json.Unmarshal(body, res)	// translates json response body into struct
 
+	// returns issue values to buffered channel for further use
 	for _ , v := range res.Data {
 		chan1 <- v.TotalResults
 	}
@@ -125,7 +132,7 @@ func pushIssues(name string, chan1 chan int) {
 
 }
 
-// Worker defines the job to be by every time a worker is added to the pool
+// Worker defines the job to be done every time a worker is added to the pool
 func Worker(name string, wg *sync.WaitGroup) {
 
 	defer wg.Done()	// removes worker from pool after completion of job
@@ -163,5 +170,14 @@ and passing by value is a lot less expensive to the memory
 Used a Buffered channel instead of passing through a normal channel because we
 don't have to create a slice or struct to pass the values between functions and maps
 are unreliable when used with goroutines
+
+*/
+
+/*
+Questions:
+
+1. Are these small efficiency's worth it in a small program?
+
+2. How do deal with Api Keys in production?
 
 */
